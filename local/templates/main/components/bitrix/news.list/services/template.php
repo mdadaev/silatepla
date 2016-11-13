205bee4f4d6a3755d70bf1ecfd6ca155d3f83131
mdadev <greatt169.92@gmail.com>
2016-11-13 11:40:20 +0300

    Тестовый коммит миши

--- local/templates/main/components/bitrix/news.list/services/template.php
+++ local/templates/main/components/bitrix/news.list/services/template.php
@@ -19,6 +19,7 @@ if (!$arResult['ITEMS']) {
         foreach ($arResult['ITEMS'] as $arItem) {
 
             // Добавляем поддержку интерфейса эрмираж
+            
 
             $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem['IBLOCK_ID'], 'ELEMENT_EDIT'));
             $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem['IBLOCK_ID'], 'ELEMENT_DELETE'), array('CONFIRM' => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
