bf05df344b84d72184fbe0f16182fca03a9a5cfd
mdadev <greatt169.92@gmail.com>
2016-12-24 12:18:19 +0300

    Добавит возможность указывать несколько разделов

--- local/templates/main/components/bitrix/news.list/partners/result_modifier.php
+++ local/templates/main/components/bitrix/news.list/partners/result_modifier.php
@@ -3,7 +3,10 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 	die();
 }
 $items = array();
-foreach( $arResult['ITEMS'] as $key => $arItem)
-    if( empty($arItem['PROPERTIES']['SECTION']['VALUE_XML_ID']) || $arItem['PROPERTIES']['SECTION']['VALUE_XML_ID'] == $arParams['SECTION'] )
+foreach( $arResult['ITEMS'] as $key => $arItem) {
+    if( empty($arItem['PROPERTIES']['SECTION']['VALUE_XML_ID']) || in_array($arParams['SECTION'], $arItem['PROPERTIES']['SECTION']['VALUE_XML_ID'])) {
         $items[$key] = $arItem;
+    }
+
+}
 $arResult['ITEMS'] = $items;
