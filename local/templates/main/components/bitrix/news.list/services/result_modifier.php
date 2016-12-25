8ac0a030108249275d2e7a0ce0e030c13549e52d
mdadev <greatt169.92@gmail.com>
2016-12-25 15:01:05 +0300

    Правки. resize image get

--- local/templates/main/components/bitrix/news.list/services/result_modifier.php
+++ local/templates/main/components/bitrix/news.list/services/result_modifier.php
@@ -2,3 +2,11 @@
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 	die();
 }
+
+foreach($arResult['ITEMS'] as &$arItem) {
+	if( !empty($arItem['PREVIEW_PICTURE']) ) {
+		$photoResize = \CFile::ResizeImageGet($arItem['PREVIEW_PICTURE']['ID'],array("width"=>600,"height"=>415),BX_RESIZE_IMAGE_EXACT);
+		$arItem['PREVIEW_PICTURE']['SRC'] = $photoResize['src'];
+	}
+}
+unset($arItem);
---
+++ local/templates/main/components/bitrix/news/projects/bitrix/news.detail/.default/result_modifier.php
@@ -0,0 +1,5 @@
+<?php
+/*if( !empty($arResult['DETAIL_PICTURE']['SRC']) ) {
+    $photoResize = \CFile::ResizeImageGet($arResult['DETAIL_PICTURE']['ID'],array("width"=>1920,"height"=>540),BX_RESIZE_IMAGE_EXACT);
+    $arResult['DETAIL_PICTURE']['SRC'] = $photoResize['src'];
+}*/
