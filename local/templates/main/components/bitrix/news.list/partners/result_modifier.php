b9aac55401efc854ccd1d71faf7e824af43d46cd
mdadev <greatt169.92@gmail.com>
2016-12-25 15:14:35 +0300

    Правки. resize image get / partners

--- local/templates/main/components/bitrix/news.list/partners/result_modifier.php
+++ local/templates/main/components/bitrix/news.list/partners/result_modifier.php
@@ -4,6 +4,13 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 }
 $items = array();
 foreach( $arResult['ITEMS'] as $key => $arItem) {
+
+    if (!empty($arItem['PREVIEW_PICTURE'])) {
+        $photoResize = \CFile::ResizeImageGet($arItem['PREVIEW_PICTURE']['ID'], array("width" => 120, "height" => 90), BX_RESIZE_IMAGE_EXACT);
+        $arItem['PREVIEW_PICTURE']['SRC'] = $photoResize['src'];
+    }
+
+
     if( empty($arItem['PROPERTIES']['SECTION']['VALUE_XML_ID']) || in_array($arParams['SECTION'], $arItem['PROPERTIES']['SECTION']['VALUE_XML_ID'])) {
         $items[$key] = $arItem;
     }
